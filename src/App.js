2102ebf055c8c94323295729360662c61d1796fc
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2021-04-20 23:04:21 +0800

    Implemented everything but sorting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import ToDoList from "./todoList";
+import { Typography } from "@material-ui/core";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Typography variant="h5">To-do List Application using React</Typography>
+                <ToDoList />
+            </header>
+        </div>
+    );
 }
 
 export default App;
