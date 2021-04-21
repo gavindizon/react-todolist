81a221afa3c887ce7a01e6e5fe85cddad37f7887
Gavin Raine Dizon <60138634+gavindizon@users.noreply.github.com>
2021-04-21 13:21:18 +0800

    added dotnet

---
+++ WebAPI/WebAPI/Migrations/20210421043324_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Tasks",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    taskName = table.Column<string>(type: "nvarchar(32)", nullable: true),
+                    date = table.Column<DateTime>(type: "DateTime2", nullable: false),
+                    priority = table.Column<int>(nullable: false),
+                    status = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Tasks", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Tasks");
+        }
+    }
+}
---
+++ WebAPI/WebAPI/Models/Task.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public class Task
+    {   
+        [Key]
+        public int id { get; set; }
+
+        [Column(TypeName = "nvarchar(32)")]
+        public string taskName { get; set; }
+
+        [Column(TypeName ="DateTime2")]
+        public DateTime date { get; set; }
+
+        [Column]
+        public int priority { get; set; }
+
+        [Column]
+        public bool status { get; set; }
+
+    }
+}
